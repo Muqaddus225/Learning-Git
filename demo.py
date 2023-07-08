d78c3169c3025ed92ffcae15e90a5e074616c67d
Muqaddus225 <mqdsjahan225@gmail.com>
2023-07-08 00:04:02 -0700

    first commit

---
+++ demo.py
@@ -0,0 +1,35 @@
+# print('fatima usman')
+
+# a=1
+# b=6
+# c=10
+# print(a+b+c)
+
+# x=(8+(5+5)-4)/2
+# print(x)
+# color='yellow';
+# if color=='yellow':
+#  print ('Mango')
+
+
+
+# perc =int(input('please enter number: '))
+# if perc <=100 and perc>=80:
+#     print("A+")
+# elif perc <80 and perc>=70:
+#   print("A")
+
+# elif perc < 70 and perc>=60:
+#  print("B")
+# elif perc <50 and perc>=40:
+#  print("D")
+# elif perc <40 and perc>=33:
+#    print("E")
+# elif perc <1 or perc>100:
+#    print("NOT APPROPRIATE")
+# else:
+#   print('FAIL')
+#   perc= int ( input('please enter number'))
+if perc<=100 and perc>=80:
+  print=("A+")
+
